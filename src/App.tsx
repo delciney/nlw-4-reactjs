b6e604a04b2f0c2191cecc6da2be6504cd029412
Delciney <delciney07@gmail.com>
2021-02-22 22:57:36 -0300

    Aula01 - Componentes, Propriedades e Estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Button } from './components/Button';
 
 function App() {
   return (
-   <h1>Hello NLW</h1>
+    <div>
+        <Button color="red">
+          Botão 1
+        </Button>
+        <Button color="blue">
+          Botão 2
+        </Button>
+        <Button color="green">
+          Botão 3
+        </Button>
+    </div>
+    
   );
 }
 
