fbc3edca880c0feea625afa36b010a05bee89416
Delciney <delciney07@gmail.com>
2021-02-25 14:00:43 -0300

    Aula04 - Adicionando audio de novo desafio

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -46,6 +46,8 @@ export const ChallengesProvider: React.FC = ({children}: ChallengesProviderProps
         
         setActiveChallenges(challenge)
 
+        new Audio('/notification.mp3').play()
+
         if (Notification.permission === 'granted'){
             new Notification('Novo desafio!',{
                 body: `Valendo ${challenge.amount} xp`
