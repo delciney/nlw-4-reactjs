92b1c84d0b9df2cf7f0ea377ee0431505cc1ce49
Delciney <delciney07@gmail.com>
2021-02-25 13:57:26 -0300

    Aula04 - Adicionando notificação web

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from "react"
+import { createContext, useState, ReactNode, useEffect } from "react"
 import challenges from "../../challenges.json"
 
 interface challengeProps {
@@ -33,14 +33,25 @@ export const ChallengesProvider: React.FC = ({children}: ChallengesProviderProps
     const [activeChallenges, setActiveChallenges] = useState(null)
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+    
+    useEffect(() => {
+        Notification.requestPermission()
+    },[])
 
     const levelUp = () => setLevel(level +1)
 
     const startNewChallenge = () => {
         const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
         const challenge = challenges[randomChallengeIndex]
-
+        
         setActiveChallenges(challenge)
+
+        if (Notification.permission === 'granted'){
+            new Notification('Novo desafio!',{
+                body: `Valendo ${challenge.amount} xp`
+            })
+        }
+          
     }
 
     const resetChallenge = () => {
